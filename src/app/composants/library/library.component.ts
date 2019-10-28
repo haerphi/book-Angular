d02286ba3ea14573b0d318a49ff0149c8523cc21
Philippe Haerens <haerphi@gmail.com>
2019-10-28 13:14:29 +0100

    remove a console.log

--- src/app/composants/library/library.component.ts
+++ src/app/composants/library/library.component.ts
@@ -9,7 +9,6 @@ import { GetbooksService } from "../../services/getbooks.service";
 export class LibraryComponent {
   constructor(public booksservice: GetbooksService) {}
   ngOnInit() {
-    console.log(this.booksservice);
     if (this.booksservice.array.length < 1) {
       this.booksservice.getbook();
     }
